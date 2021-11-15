ac58d645f90f99d3f442a2107cb2202f21f24a72
tonykino <tony.kino.3@gmail.com>
2021-11-15 16:06:38 +0100

    Fix - map is_open didn't work properly

--- srcs/map/is_open.c
+++ srcs/map/is_open.c
@@ -6,7 +6,7 @@
 /*   By: tokino <tokino@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 16:33:42 by tokino            #+#    #+#             */
-/*   Updated: 2021/11/14 17:07:29 by tokino           ###   ########.fr       */
+/*   Updated: 2021/11/15 16:05:34 by tokino           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ bool	is_open(t_map *map)
 		col = 0;
 		while (col < map->num_cols)
 		{
-			if (map->grid[row][col] == '0' && \
+			if (ft_strchr("0NSEW", map->grid[row][col]) && \
 				(map_is_open_on_top(map, row, col) || \
 				map_is_open_on_bottom(map, row, col) || \
 				map_is_open_on_left(map, row, col) || \
@@ -40,22 +40,24 @@ bool	is_open(t_map *map)
 
 bool	map_is_open_on_left(t_map *map, int row, int col)
 {
-	return (col > 0 && ft_strchr("01NSEW", map->grid[row][col - 1]) == NULL);
+	return (col == 0 || \
+		(col > 0 && ft_strchr("01NSEW", map->grid[row][col - 1]) == NULL));
 }
 
 bool	map_is_open_on_right(t_map *map, int row, int col)
 {
-	return (col < map->num_cols - 1 && \
-		ft_strchr("01NSEW", map->grid[row][col + 1]) == NULL);
+	return (col == map->num_cols - 1 || (col < map->num_cols - 1 && \
+		ft_strchr("01NSEW", map->grid[row][col + 1]) == NULL));
 }
 
 bool	map_is_open_on_bottom(t_map *map, int row, int col)
 {
-	return (row < map->num_rows - 1 && \
-		ft_strchr("01NSEW", map->grid[row + 1][col]) == NULL);
+	return (row == map->num_rows - 1 || (row < map->num_rows - 1 && \
+		ft_strchr("01NSEW", map->grid[row + 1][col]) == NULL));
 }
 
 bool	map_is_open_on_top(t_map *map, int row, int col)
 {
-	return (row > 0 && ft_strchr("01NSEW", map->grid[row - 1][col]) == NULL);
+	return (row == 0 || \
+		(row > 0 && ft_strchr("01NSEW", map->grid[row - 1][col]) == NULL));
 }
